3bed8c042044ff92084090bb43d2d237d71fa66a
Hillevi Eklöw <hillevieklow@gmail.com>
2018-03-26 22:45:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(array){
-  new_array = array.appendKitten("Ralph")
-  return new_array
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
 }
